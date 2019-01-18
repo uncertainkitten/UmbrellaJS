104a13464f6c89c82bf8d5dd9ed0749ca4718f31
Cassandra <uncertainkitten@gmail.com>
2019-01-17 17:06:52 -0800

    Add React Router, create NavBar component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Splash from './components/splash';
+import NavBar from './components/navbar';
+import {HashRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Splash />
-      </div>
+      <HashRouter>
+        <div className="App">
+          <NavBar labels={["Home", "About", "FAQ", "NITRO CHAT"]} />
+          <Splash />
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class NavBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      linkArray: []
+    }
+  }
+
+  componentDidMount(){
+    this.buildArray();
+  }
+
+  buildArray(){
+    let newLinks = [];
+    this.props.labels.forEach(link => {
+      newLinks.push(<Link className="nav-link" to={`/${link}`}> {link}</Link>);
+    });
+    this.setState({linkArray: newLinks});
+  }
+
+  render(){
+    return(
+      <div className="nav-bar-container">
+        {this.state.linkArray}
+      </div>
+    )
+  }
+}
+
+export default NavBar;
