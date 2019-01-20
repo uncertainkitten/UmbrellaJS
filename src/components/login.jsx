f6b16a8d4448369074dc8c91a5a4fc343afeb658
Cassandra <uncertainkitten@gmail.com>
2019-01-20 02:14:29 -0800

    Style login, fix state behavior

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -14,10 +14,12 @@ class LoginForm extends React.Component{
   }
 
   buildFormArray(){
+    //All fields should be passed to the form as a prop "fields" - this will splay them out.
+    // Password gets hardcoded due to being a unique field.  Arguably, I should do the same with email.
     let newFields = [];
     this.props.fields.forEach( field => {
       newFields.push(<label className="login-label">{field[0].toUpperCase() + field.substring(1)}</label>)
-      newFields.push(<input className="login-input" type="text" onChange={this.update({field})} value={this.state[field]} />)
+      newFields.push(<input className="login-input" type="text" onChange={this.update(field)} value={this.state[field]} />)
     });
     newFields.push(<label className="login-label">Password</label>);
     newFields.push(<input className="login-input" type="password" onChange={this.update("password")} value={this.state.password} />)
@@ -36,7 +38,9 @@ class LoginForm extends React.Component{
     this.props.fields.forEach( field => {
       formSubmit[field] = this.state[field]
     });
+    formSubmit.password = this.state.password;
     console.log(formSubmit);
+    //Note - that console.log should actually be an API calls that sends the information to the server
   }
 
   render(){
